bf44e53154cf328b4914f1f8bc465ae9f5686395
Fahad albgumi <albgumifahd@gmail.com>
2019-11-12 17:47:08 +0300

    Initial Commit

---
+++ OnTheMap/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  OnTheMap
+//
+//  Created by fahad on 15/03/1441 AH.
+//  Copyright © 1441 Fahad Albgumi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
